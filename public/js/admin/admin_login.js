9842bbb74e3723e2a9d3ffa33913744d8047f5ae
Jovin Mwilanga <jovinbeda@hotmail.com>
2015-03-29 22:05:43 -0400

    temp host shift changes

--- public/js/admin/admin_login.js
+++ public/js/admin/admin_login.js
@@ -16,22 +16,4 @@ $(document).ready(function () {
             }
         });
     }
-
-    function getUserUniqueCuid() {
-        $.ajax({
-            url: "/api/getUserUniqueCuid",
-            type: "GET",
-            success: function (resp) {
-                if (document.location.hostname.search("harvardgrill") !== -1) {
-                    ga('set', '&uid', resp.userUniqueCuid); // Set the user ID using signed-in user_id.
-                }
-            },
-            error: function (resp) {
-                //the resp is an error string from the database
-                console.log("Error while getting user uniqueCuid, err = " + resp);
-            }
-        });
-    }
-
-    getUserUniqueCuid();
 });
